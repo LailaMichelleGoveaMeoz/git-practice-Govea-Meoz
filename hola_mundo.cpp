00c5479e98983829483ecd573b8aa3274ce24bd6
Laila Michelle Govea Meoz <lailagovea@gmail.com>
2025-09-24 23:23:51 -0400

    actualizar archivos

--- hola_mundo.cpp
+++ hola_mundo.cpp
@@ -6,6 +6,6 @@ int main() {
     cout << "¡Hola Mundo desde Git!" << endl;
     cout << "Mi nombre es: Laila" << endl;
     cout << "Estoy aprendiendo Git en Programación II" << endl;
-   
+    cout << "Este es mi segundo commit!" << endl;
     return 0;
 }
